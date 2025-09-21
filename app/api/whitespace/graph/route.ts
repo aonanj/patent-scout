580487f03859cd53ed414e87ba901a824711fe7f
aonanj <admin@spurly.io>
2025-09-20 17:13:20 -0700

    fix 405 error

--- app/api/whitespace/graph/route.ts
+++ app/api/whitespace/graph/route.ts
@@ -1,19 +1,22 @@
 // app/api/whitespace/graph/route.ts
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const authHeader = req.headers.get('authorization');
   const headers = new Headers();
   if (authHeader) {
     headers.append("Authorization", authHeader);
   }
+  headers.append("Content-Type", "application/json");
 
-  const searchParams = req.nextUrl.search;
-  const url = `${process.env.BACKEND_URL}/whitespace/graph${searchParams}`;
+  const body = await req.json();
+  const url = `${process.env.BACKEND_URL}/whitespace/graph`;
 
   try {
     const r = await fetch(url, {
+      method: "POST",
       headers: headers,
+      body: JSON.stringify(body),
       cache: "no-store"
     });
     const t = await r.text();
